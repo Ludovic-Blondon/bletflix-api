00ecdf9da3c28be16e57741aa18e6bfedd449542
Ludovic <ludovic.mmcreation@com>
2020-04-12 18:37:17 +0200

    update work entity

--- src/Entity/Work.php
+++ src/Entity/Work.php
@@ -33,6 +33,21 @@ class Work
      */
     private $mediaObject;
 
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $languages = [];
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $subtitles = [];
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $country;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -70,6 +85,42 @@ class Work
     public function setMediaObject(?MediaObject $mediaObject): self
     {
         $this->mediaObject = $mediaObject;
+
+        return $this;
+    }
+
+    public function getLanguages(): ?array
+    {
+        return $this->languages;
+    }
+
+    public function setLanguages(array $languages): self
+    {
+        $this->languages = $languages;
+
+        return $this;
+    }
+
+    public function getSubtitles(): ?array
+    {
+        return $this->subtitles;
+    }
+
+    public function setSubtitles(?array $subtitles): self
+    {
+        $this->subtitles = $subtitles;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
 
         return $this;
     }
